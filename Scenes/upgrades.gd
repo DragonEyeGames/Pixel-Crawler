9de9b72eca4293bdc312cfe7d8e4f99a053e94fd
Taxalottle Syndicate <133176852+DragonEyeGames@users.noreply.github.com>
2025-12-17 11:55:44 -0500

    More Menu Work
    I did some more work on the menu selection stuff.

--- Scenes/upgrades.gd
+++ Scenes/upgrades.gd
@@ -1,13 +1,33 @@
 extends Control
 
+enum selectedTypes {Damage, Speed, Health}
+var selected:selectedTypes
 
 func _damage() -> void:
-	pass # Replace with function body.
+	$Free/Damage.color = Color.DARK_GREEN
+	match selected:
+		selectedTypes.Speed:
+			$Free/Speed.color = Color.BLACK
+		selectedTypes.Health:
+			$Free/Health.color = Color.BLACK
+	selected=selectedTypes.Damage
 
 
 func _speed() -> void:
-	pass # Replace with function body.
+	$Free/Speed.color = Color.DARK_GREEN
+	match selected:
+		selectedTypes.Damage:
+			$Free/Damage.color = Color.BLACK
+		selectedTypes.Health:
+			$Free/Health.color = Color.BLACK
+	selected=selectedTypes.Speed
 
 
 func _health() -> void:
-	pass # Replace with function body.
+	$Free/Health.color = Color.DARK_GREEN
+	match selected:
+		selectedTypes.Speed:
+			$Free/Speed.color = Color.BLACK
+		selectedTypes.Damage:
+			$Free/Damage.color = Color.BLACK
+	selected=selectedTypes.Health
