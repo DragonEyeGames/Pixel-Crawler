af7726e298241fbd03372515f34aa97581e5770d
Taxalottle Syndicate <bthole41@gmail.com>
2025-12-17 20:25:03 -0500

    More Shop Features
    I continued working on the shop! Some cool features coming soon!

--- Scenes/upgrades.gd
+++ Scenes/upgrades.gd
@@ -3,6 +3,11 @@ extends Control
 enum selectedTypes {Damage, Speed, Health}
 var selected:selectedTypes
 
+func _ready() -> void:
+	$Control/VBoxContainer/Health.text="Health: " + str(int(GameManager.playerMaxHealth))
+	$Control/VBoxContainer/Speed.text="Speed: " + str(int(GameManager.playerSpeed))
+	$Control/VBoxContainer/Strength.text="Strength: " + str(int(GameManager.playerStrength))
+
 func _damage() -> void:
 	$Free/Damage.color = Color.DARK_GREEN
 	match selected:
@@ -31,3 +36,15 @@ func _health() -> void:
 		selectedTypes.Damage:
 			$Free/Damage.color = Color.BLACK
 	selected=selectedTypes.Health
+
+
+func _on_continue_pressed() -> void:
+	match selected:
+		selectedTypes.Health:
+			GameManager.playerMaxhealth+=1
+			GameManager.playerHealth+=1
+		selectedTypes.Damage:
+			GameManager.playerStrength+=1
+		selectedTypes.Speed:
+			GameManager.playerSpeed+=1
+	get_tree().change_scene_to_file("res://Scenes/shop.tscn")
