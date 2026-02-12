ffcb5e5f9faa4952ea9b5cec328c8349b7225c4b
Taxalottle Syndicate <bthole41@gmail.com>
2026-02-11 20:24:05 -0500

    Second Attack
    I gave the axeman a second attack. It does damage in a circle but has a really high cooldown.

--- Scripts/axeman.gd
+++ Scripts/axeman.gd
@@ -20,13 +20,14 @@ func _physics_process(_delta: float) -> void:
 		attacking=true
 		sprite.speed_scale=attackSpeed
 		animator.speed_scale=attackSpeed
-		if(randi_range(1, 10)==10):
-			sprite.play("attack-2")
-			animator.play("attack-2")
-		else:
-			sprite.play("attack-1")
-			animator.play("attack-1")
-			
+		sprite.play("attack-1")
+		animator.play("attack-1")
+	if(Input.is_action_just_pressed("Attack-2") and attacking==false):
+		attacking=true
+		sprite.speed_scale=attackSpeed
+		animator.speed_scale=attackSpeed
+		sprite.play("attack-2")
+		animator.play("attack-2")
 		
 func flip(newDirection: String):
 	direction=newDirection
@@ -41,11 +42,17 @@ func flip(newDirection: String):
 
 
 func _on_player_animation_finished() -> void:
-	if("attack" in sprite.animation):
+	if("attack-1" == sprite.animation):
 		attacking=false
 		sprite.speed_scale=1.0
 		animator.speed_scale=1.0
 		sprite.play("idle")
+	if("attack-2" == sprite.animation):
+		sprite.speed_scale=1.0
+		animator.speed_scale=1.0
+		sprite.play("idle")
+		await get_tree().create_timer(.4).timeout
+		attacking=false
 
 
 func _enemy_hit(area: Area2D) -> void:
--- Scripts/chest.gd
+++ Scripts/chest.gd
@@ -9,6 +9,7 @@ func _process(_delta: float) -> void:
 	if(collided and Input.is_action_just_pressed("Interact") and not opened):
 		opened=true
 		play("open")
+		$Open.emitting=true
 		GameManager.playerGold+=randi_range(gold-5, gold+5)
 
 func _on_area_2d_area_entered(_area: Area2D) -> void:
