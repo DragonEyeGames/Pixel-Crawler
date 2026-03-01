5bf42a0cf80011872f90c4a19d119e8360d91a4a
Taxalottle Syndicate <bthole41@gmail.com>
2026-03-01 11:17:13 -0500

    Improved Fire
    I made the fire better by giving them all a changing glow and a sound effect.

--- Scenes/torch.gd
+++ Scenes/torch.gd
@@ -1,11 +1,11 @@
 extends AnimatedSprite2D
 
-
+@export var multiplier = .3
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$AudioStreamPlayer2D.volume_db*=1.3
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	$PointLight2D.energy=$PointLight2D.texture_scale/2
+	$PointLight2D.energy=$PointLight2D.texture_scale*multiplier
