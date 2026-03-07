522fb9f48d37dc915db9b5b49c9c93b56efb193e
Taxalottle Syndicate <bthole41@gmail.com>
2026-03-07 18:06:16 -0500

    Improved Menu
    I improved the menu with buttons and am working on a settings panel.

--- MainMenu.gd
+++ MainMenu.gd
@@ -12,8 +12,39 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/characterSelect.tscn")
 
 
 func _on_settings_pressed() -> void:
-	pass # Replace with function body.
+	if($SettingsMenu.modulate.a!=1):
+		var tween=create_tween()
+		tween.tween_property($SettingsMenu, "modulate:a", 1, .1)
+	
+
+
+func _on_start_mouse_entered() -> void:
+	var tween=create_tween()
+	tween.tween_property($Start/FireSprite2, "modulate:a", 1, .1)
+	var tween2=create_tween()
+	tween2.tween_property($Start, "scale", Vector2(8.668, 7.216), .1)
+
+
+func _on_start_mouse_exited() -> void:
+	var tween=create_tween()
+	tween.tween_property($Start/FireSprite2, "modulate:a", 0, .1)
+	var tween2=create_tween()
+	tween2.tween_property($Start, "scale", Vector2(7.88, 6.56), .1)
+
+
+func _on_settings_mouse_entered() -> void:
+	var tween=create_tween()
+	tween.tween_property($Settings/FireSprite2, "modulate:a", 1, .1)
+	var tween2=create_tween()
+	tween2.tween_property($Settings, "scale", Vector2(8.668, 7.216), .1)
+
+
+func _on_settings_mouse_exited() -> void:
+	var tween=create_tween()
+	tween.tween_property($Settings/FireSprite2, "modulate:a", 0, .1)
+	var tween2=create_tween()
+	tween2.tween_property($Settings, "scale", Vector2(7.88, 6.56), .1)
