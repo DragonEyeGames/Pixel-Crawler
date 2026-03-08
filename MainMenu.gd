953541b220af6b6911839f78df9808b75d873748
Taxalottle Syndicate <bthole41@gmail.com>
2026-03-08 11:39:34 -0400

    Audio Busses
    I started working on audio busses. I need to go and import the SFX bus to all of the lil things now.

--- MainMenu.gd
+++ MainMenu.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$SettingsMenu/Close.modulate=Color.WEB_GRAY
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,6 +17,7 @@ func _on_start_pressed() -> void:
 
 func _on_settings_pressed() -> void:
 	if($SettingsMenu.modulate.a!=1):
+		$SettingsMenu.visible=true
 		var tween=create_tween()
 		tween.tween_property($SettingsMenu, "modulate:a", 1, .1)
 	
@@ -48,3 +49,46 @@ func _on_settings_mouse_exited() -> void:
 	tween.tween_property($Settings/FireSprite2, "modulate:a", 0, .1)
 	var tween2=create_tween()
 	tween2.tween_property($Settings, "scale", Vector2(7.88, 6.56), .1)
+
+
+func _on_close_mouse_entered() -> void:
+	var tween=create_tween()
+	tween.tween_property($SettingsMenu/Close, "modulate", Color.WHITE, .1)
+
+
+func _on_close_mouse_exited() -> void:
+	var tween=create_tween()
+	tween.tween_property($SettingsMenu/Close, "modulate", Color.WEB_GRAY, .1)
+
+
+func _on_close_pressed() -> void:
+	if($SettingsMenu.modulate.a!=0):
+		var tween=create_tween()
+		tween.tween_property($SettingsMenu, "modulate:a", 0, .1)
+	await get_tree().create_timer(.1).timeout
+	await get_tree().process_frame
+	$SettingsMenu.visible=false
+
+
+func _on_sfx_bar_value_changed(value: float) -> void:
+	var multiplier=1
+	if(value<0):
+		value=abs(value)
+		multiplier=-1
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), sqrt(value)*multiplier)
+
+
+func _on_music_bar_value_changed(value: float) -> void:
+	var multiplier=1
+	if(value<0):
+		value=abs(value)
+		multiplier=-1
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), sqrt(value)*multiplier)
+
+
+func _on_master_bar_value_changed(value: float) -> void:
+	var multiplier=1
+	if(value<0):
+		value=abs(value)
+		multiplier=-1
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), sqrt(value)*multiplier)
