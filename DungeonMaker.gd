c88e8acdca2b33eff3661672d9599b8d684c58ee
Taxalottle Syndicate <bthole41@gmail.com>
2026-03-09 17:16:34 -0400

    End + Gong
    I made is so that when the level is done generating there is a cool gong sound effect. The exit level is also generated every time now. There is now an escape.

--- DungeonMaker.gd
+++ DungeonMaker.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @export var levels: Array[PackedScene]
+@export var end: PackedScene
+var instantiatedEnd
 @export var caps: Array[PackedScene]
 @export var startingLevel: Node2D
 var instantiatedLevels=[]
@@ -8,6 +10,7 @@ var instantiatedCaps=[]
 var placedLevels=[] #The levels that have already been moved and generated n stuff
 var uniqueLeft=[]
 var failedCaps=0
+var endPlaced=false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for level in levels:
@@ -21,6 +24,9 @@ func _ready() -> void:
 		var newCap = capScene.instantiate()
 		newCap.visible=false
 		instantiatedCaps.append(newCap)
+	var newEnd = end.instantiate()
+	newEnd.visible=false
+	instantiatedEnd=newEnd
 	generateWorld()
 		
 
@@ -154,11 +160,64 @@ func cap():
 			door.connection=newDoor
 			newDoor.connection=door
 
+func placeEnd():
+	var level=null
+	while level==null:
+		if(len(placedLevels)<=0):
+			return
+		var testLevel = placedLevels.pick_random()
+		if(testLevel.is_in_group("North")):
+			level=testLevel
+	var door = null
+	var north_doors = []
+	for d in level.doors:
+		if d.direction == "north":
+			north_doors.append(d)
+	if north_doors.is_empty():
+		#generate(placedLevels.pick_random())1
+		return
+	door = north_doors.pick_random()
+	level.doors.erase(door)
+	if(door.direction=="north"):
+		var levelReplacement=instantiatedEnd
+		add_child(levelReplacement)
+		var newDoor=null
+		var south_doors = []
+
+		for d in levelReplacement.doors:
+			if d.direction == "south":
+				south_doors.append(d)
+
+		if south_doors.is_empty():
+			#generate(placedLevels.pick_random())
+			return
+
+		newDoor = south_doors.pick_random()
+		levelReplacement.doors.erase(newDoor)
+		levelReplacement.global_position = door.global_position - newDoor.position
+		await get_tree().process_frame
+		await get_tree().process_frame
+		if(levelReplacement.colliding):
+			level.colliding=false
+			levelReplacement.colliding=false
+			remove_child(levelReplacement)
+			level.doors.append(door)
+			levelReplacement.doors.append(newDoor)
+			#placedLevels.erase(levelReplacement)
+			#generate(placedLevels.pick_random())
+		else:
+			door.connection=newDoor
+			newDoor.connection=door
+			endPlaced=true
+
 func generateWorld():
 	await generate(startingLevel)
 	await get_tree().physics_frame
 	while len(uniqueLeft)>0:
 		await generate(placedLevels.pick_random())
+	await get_tree().process_frame
+	while endPlaced==false:
+		await placeEnd()
 	await get_tree().process_frame
 	while len(instantiatedCaps)>=1 and failedCaps<=100:
 		await cap()
@@ -166,3 +225,4 @@ func generateWorld():
 	await get_tree().process_frame
 	startingLevel.modulate.a=1
 	SignalBus.generated.emit()
+	$Gong.play()
