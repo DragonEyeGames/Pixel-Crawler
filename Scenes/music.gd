c7b281ca3d07c23745421a8803d8382de0a67b5e
Taxalottle Syndicate <bthole41@gmail.com>
2026-03-14 15:27:30 -0400

    Music Fix
    I put the music back together. Its not quite as good in web, but is close enough.

--- Scenes/music.gd
+++ Scenes/music.gd
@@ -8,13 +8,19 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	$Paused.seek($Music.get_playback_position())
-	print("music:", $Music.get_playback_position(), " paused:", $Paused.get_playback_position())
+	if get_tree().paused:
+		$Paused.seek($Music.get_playback_position())
 	if(backupPause!=get_tree().paused):
 		backupPause=get_tree().paused
 		if(get_tree().paused):
+			print("paused")
+			print("music:", $Music.get_playback_position(), " paused:", $Paused.get_playback_position())
 			$Music.volume_db=-80
 			$Paused.volume_db=-27.67
+			var pos = $Music.get_playback_position()
+			#$Music.stop()
+			$Paused.seek(pos)
+
 		else:
 			$Music.volume_db=-27.67
 			$Paused.volume_db=-80
